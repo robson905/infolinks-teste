04eb5f2428a6d62c44286da7080e861d58125c0c
Robson <monteirorobson016@gmail.com>
2025-04-05 15:58:46 -0300

    historia de pescador

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,6 +17,9 @@ return (
     <Text>Sou Nelpazense, tenho 500 anos e meu papel no mundo é pescar peixe boi, 
     viajo comendo farinha puba com peixe boi. Minha incrivel experiencia foi pescar um  peixe boi de tarrafa</Text>
 
+    <Text>Tô em busca do dinheiro do pescador aqui no brasil</Text>
+    
+
 </View>
 );
 }
