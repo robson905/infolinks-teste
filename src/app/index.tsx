b4ca7af282499a3861f4986eb8bd90d06726270f
Robson <monteirorobson016@gmail.com>
2025-04-05 15:40:24 -0300

    Novos Dados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,9 +4,16 @@ export default function index () {
 return (
 <View>
     <Text>Meu cadastro!!</Text>
-    <Text>Robson Monteiro de Lima</Text>
+    <Text>Robson Monteiro</Text>
     <Text>Nascimento: 12/08</Text>
     <Text>Endereço: Rua tal</Text>
+    <Text>Cidade: Boca do Acre</Text>
+    <Text>Bairro: Macaxeiral</Text>
+    <Text>Telefone: 97999000999</Text>
+    <Text>Email: email.com</Text>
+    <Text>Nacionalidade: Nepal</Text>
+    <Text>Altura: três metro</Text>
+    <Text>Idade: 500 anos</Text>
 
 </View>
 );
